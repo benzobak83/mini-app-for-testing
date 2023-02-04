6e70353ae9b8c993eb719eb066ec7a62d329a6c6
benzobak83 <vitanaolord@gmail.com>
2023-02-04 04:48:50 +0300

    feat: lazy-loading

--- src/components/ProductComponents/Gallery/ProductGalleryItem/ProductGalleryItem.tsx
+++ src/components/ProductComponents/Gallery/ProductGalleryItem/ProductGalleryItem.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 import { ProductGalleryItemProps } from './ProductGalleryItem.types'
 
 const ProductGalleryItem: FC<ProductGalleryItemProps> = ({ imgSrc }) => {
-  return <img src={imgSrc} alt="product-img" className="gallery__img" />
+  return <img src={imgSrc} alt="product-img" className="gallery__img" loading='lazy'/>
 }
 
 export { ProductGalleryItem }
